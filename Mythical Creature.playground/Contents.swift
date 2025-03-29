24bc71a3f3472a694c7f2c8324b9de7ebc5ea3c1
Mohammad Hafzah <git@hafzah.io>
2025-03-30 01:41:56 +0300

    submit part 1: creating structs

--- Mythical Creature.playground/Contents.swift
+++ Mythical Creature.playground/Contents.swift
@@ -1,1 +1,12 @@
+// Part 1: Creating Structs
 
+struct Creature {
+  let name: String
+  let description: String
+  let isGood: Bool
+  var magicPower: Int
+}
+
+var astralHorn = Creature(name: "Astral Horn", description: "A unicorn", isGood: true, magicPower: 9)
+var infernoDrake = Creature(name: "Astral Horn", description: "A dragon", isGood: false, magicPower: 8)
+var emberSoul = Creature(name: "Astral Horn", description: "A phoenix", isGood: true, magicPower: 6)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+Homework
+
+Introduction: This is a submittal of the second homework assignment, it is related to the second learning point: "Introduction to Swift: Homework Assignment for Beginning iOS & Swift" of the "Foundational Tools in iOS" learning path.
+
+Purpose: This repository is for grading and feedback purposes.
+
+Breakdown of homework:
+1- Part 1: Creating Structs (made on branch: creature-structure)
+
+Thank you.
 
