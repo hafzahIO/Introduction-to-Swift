cc7db0bf90caef0df9d30af391004bcdd31155b0
Mohammad Hafzah <git@hafzah.io>
2025-03-31 23:15:04 +0300

    submit part 4: mythical creature interactions

--- Mythical Creature.playground/Contents.swift
+++ Mythical Creature.playground/Contents.swift
@@ -28,6 +28,57 @@ struct Creature {
     return Int(numerator / denominator)
   }
   
+//  // Part 4: Mythical Creature Interactions
+//  func interactWith(creature: Creature) {
+//    
+//    switch (self.isGood, creature.isGood) {
+//    case (false, false):
+//      let combinedPowers = self.magicPower + creature.magicPower
+//      print("Both creatures are evil and have a combined magic power of\(combinedPowers).")
+//    
+//    case (false, true):
+//      let combinedPowers = self.magicPower - creature.magicPower
+//      if combinedPowers > 0 {
+//        print("To beat the evil \(self.name), you have to increase \(creature.name)'s magic power by more than \(abs(combinedPowers)) points.")
+//      } else if combinedPowers < 0 {
+//        print("\(creature.name) is strong enough to beat \(self.name). Look out for any unexpected evil backup")
+//      } else {
+//        print("Both creatures are equally strong. Find a way to increase \(creature.name)'s chances of winning.")
+//      }
+//    
+//    case (true, false):
+//      let combinedPowers = self.magicPower - creature.magicPower
+//      if combinedPowers > 0 {
+//        print("\(self.name) is strong enough to beat \(creature.name). Look out for any unexpected evil backup")
+//      } else if combinedPowers < 0 {
+//        print("To beat the evil \(creature.name), you have to increase \(self.name)'s magic power by more than \(abs(combinedPowers)) points.")
+//      } else {
+//        print("Both creatures are equally strong. Find a way to increase \(self.name)'s chances of winning.")
+//      }
+//    
+//    default:
+//      let combinedPowers = self.magicPower + creature.magicPower
+//      print("Both creatures are good and have a combined magic power of\(combinedPowers).")
+//    }
+//  }
+  
+  // Part 4: Mythical Creature Interactions
+  func interactWith(creature: Creature) {
+    switch (self.isGood, creature.isGood) {
+    case (false, false):
+      print("Both creatures are evil; it's good that they are fighting each other.")
+    
+    case (false, true):
+      print("The evil \(self.name) is fighting the good \(creature.name). Do you think we can convince \(self.name) to turn to the right side?")
+      
+    case (true, false):
+      print("I guess there is no better time to fight the evil \(creature.name), right?!")
+    
+    default:
+      print("Both creatures are good and there is no point in them fighting each other. What can we do?")
+    }
+  }
+  
 }
 
 
@@ -45,10 +96,30 @@ creatureCatalog.append(astralHorn)
 creatureCatalog.append(infernoDrake)
 creatureCatalog.append(emberSoul)
 
+// Part 3: The Mythical Creature
 func describeCreature(creatureArray: [Creature]) {
   for creature in creatureArray {
     print("\(creature.name) is a \(creature.description) and has \(creature.ability)")
+    
+    // Part 4: Mythical Creature Interactions
+    print("\(creature.interactWith(creature: creatureArray.randomElement()!))")
+  }
+}
+// Examples related to part 4: Mythical Creature Interactions
+describeCreature(creatureArray: creatureCatalog)
+astralHorn.interactWith(creature: infernoDrake)
+
+
+
+print("\n--- this is the end of the homework ---\n")
+
+/* The homework is done before this line. However, the func describeCreature has 2 print functions to deliver what I was asked of. If I want to combine the content of the two print functions into one, what is printed inside the console down is done in reverse (i.e. it calls the interactWith function first then the content of the first print function). I don't know why is this and I hope you can help understand this behavior (this is why I separated the content into 2 separate print functions).
+ */
+
+func describeCreature2(creatureArray: [Creature]) {
+  for creature in creatureArray {
+    print("\(creature.name) is a \(creature.description) and has \(creature.ability) \(creature.interactWith(creature: creatureArray.randomElement()!))")
   }
 }
 
-describeCreature(creatureArray: creatureCatalog)
+describeCreature2(creatureArray: creatureCatalog)
